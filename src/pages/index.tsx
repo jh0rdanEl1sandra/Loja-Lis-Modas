34c16ed2a1fd2090c4397ed16cbf01e27a276230
jh0rdanEl1sandra <113987582+jh0rdanEl1sandra@users.noreply.github.com>
2022-09-28 16:21:10 -0300

    Home Page alterada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,12 @@ import Nav from '../components/navbar/nav';
 export default function Home() {
   return (
     <main>
-      <Nav />
+      <div>
+        <Nav />
+        <div className={styles.text}>
+          <h1>Seja Bem Vindo!</h1>
+        </div>
+      </div>
       <div className={styles.wallpLis}></div>
       <div className={styles.wallp}></div>
     </main>
